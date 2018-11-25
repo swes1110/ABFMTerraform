ce7f7d5dd9a739d1b71ec556a4bdb9d52ae80150
Shawn Reynolds <sreynolds@theabfm.org>
2018-11-25 10:46:53 -0500

    Remove dynamic resource name

---
+++ ResourceGroup.tf
@@ -0,0 +1,8 @@
+variable "resourcename" {}
+variable "rgname" {}
+variable "location" {}
+
+resource "azurerm_resource_group" "resourceGroup" {
+  name      =   "${var.rgname}"
+  location  =   "${var.location}"
+}
