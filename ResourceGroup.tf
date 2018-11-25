e661ddde7232624a8c685340bfdcfe66f2529558
Shawn Reynolds <sreynolds@theabfm.org>
2018-11-25 10:51:59 -0500

    Remove unneeded variable

--- ResourceGroup.tf
+++ ResourceGroup.tf
@@ -1,12 +1,7 @@
-variable "resourcename" {}
 variable "rgname" {}
 variable "location" {}
 
-<<<<<<< HEAD
 resource "azurerm_resource_group" "resourceGroup" {
-=======
-resource "azurerm_resource_group" "${var.resourcename}" {
->>>>>>> 5a70b57d8967019320fd06d90c7413ed09b2a079
   name      =   "${var.rgname}"
   location  =   "${var.location}"
 }
